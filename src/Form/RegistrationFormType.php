019833712510d45490d2cc2327c74c5e6978cb75
Bernadetta Sibe <bernikw@gmail.com>
2022-04-11 14:32:17 +0200

    Styling form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -7,6 +7,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -18,15 +20,26 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('pseudo')
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
+            ->add('pseudo', TextType::class, [
+                'attr' => [
+                    'class'=>'form-control'
+                ],
+                'label' =>'Votre Pseudo'
+            ])
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'class'=>'form-control'
+                ],
+                'label' =>'E-mail'           
+            ])
+            ->add('RgpdConsent', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
                     ]),
                 ],
+                'label' =>'J\'accepte la politique de confidentialité'
             ])
           /*  ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
@@ -48,10 +61,11 @@ class RegistrationFormType extends AbstractType
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'invalid_message' => 'The password fields must match.',
-                'options' => ['attr' => ['class' => 'password-field']],
+                'options' => ['attr' => ['class' => 'password-field',
+                'class'=> 'form-control']],
                 'required' => true,
-                'first_options'  => ['label' => 'Password'],
-                'second_options' => ['label' => 'Repeat Password'],
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmation du mot de passe'],
             ])
         ;
     }
