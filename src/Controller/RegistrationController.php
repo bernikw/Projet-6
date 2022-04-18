719fa329cb4a466c7d028359142422fe3be8384d
Bernadetta Sibe <bernikw@gmail.com>
2022-04-18 19:20:21 +0200

    Add send email, add form forgot password and form reset password

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -13,6 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use App\Repository\UserRepository;
 
 
 
@@ -42,14 +43,7 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            // generate a signed url and email it to the user
-           /* $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-                (new TemplatedEmail())
-                    ->from(new Address('snowtricks@gmail.com', 'SnowTricks'))
-                    ->to($user->getEmail())
-                    ->subject('Please Confirm your Email')
-                    ->htmlTemplate('registration/confirmation_email.html.twig')
-            );*/
+        
             // do anything else you need here, like send an email
             $mail->send(
                 'snowtricks@gmail.com',
@@ -59,34 +53,43 @@ class RegistrationController extends AbstractController
                 compact('user')
             );
 
-            return $userAuthenticator->authenticateUser(
-                $user,
-                $authenticator,
-                $request
+            $this->addFlash('success',
+                "Votre compte a été crée ! Veuillez activer votre compte en cliquant sur le lien qui vous a été envoyé par e-mail !"
             );
+
+            return $this->redirectToRoute('app_login');
+            
+          
         }
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
     }
+/*
+    #[Route('/verify_account/{activatedToken}', name: 'app_verify_account')]
 
-   /* #[Route('/verify/email', name: 'app_verify_email')]
-    public function verifyUserEmail(Request $request, TranslatorInterface $translator): Response
+    public function verifyAccount(UserRepository $userRepository, $validatedToken,  EntityManagerInterface $entityManager): Response
     {
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $user = $userRepository->find($validatedToken);
 
-        // validate email confirmation link, sets User::isVerified=true and persists
-        try {
-            $this->emailVerifier->handleEmailConfirmation($request, $this->getUser());
-        } catch (VerifyEmailExceptionInterface $exception) {
-            $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
+        if($validatedToken !== null && $validatedToken === $user->getValidatedToken()){
 
-            return $this->redirectToRoute('app_');
+            $user->setActivated(true);
+            $entityManager->flush($user);
+
+            $this->addFlash(
+                'success',
+                "Votre compte a été activé avec succès ! Vous pouvez désormais vous connecter !"
+            );
+
+        } else{
+
+            $this->addFlash(
+                'danger',
+                "La validation de votre compte a échoué. Le lien de validation a expiré !"
+            );  
         }
-
-        // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
 
         return $this->redirectToRoute('app_home');
     }*/
---
+++ src/Form/ForgotPasswordType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+
+class ForgotPasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', RepeatedType::class, [
+                'type' => EmailType::class,
+                'invalid_message' => 'Les adresses e-mail ne sont pas identiques.',
+                'required' => true,
+                'first_options'  => ['label' => 'Saisir votre adress e-mail'],
+                'second_options' => ['label' => 'Confirmez votre adress e-mail'],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+
+         'data_class'=> User::class,
+
+        ]);
+    }
+
+}
