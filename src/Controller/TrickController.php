9a4502e53b634958044b960dc8b67ffdb90e1a8c
Bernadetta Sibe <bernikw@gmail.com>
2022-05-01 23:28:55 +0200

    Add pages create and edit trick, and add route delete trick

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -7,6 +7,7 @@ use App\Form\TrickType;
 use App\Repository\TrickRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,52 +17,100 @@ class TrickController extends AbstractController
     #[Route('/', name: 'app_list')]
     public function list(TrickRepository $trickRepository): Response
     {
-        
-        return $this->render('trick/list.html.twig',[ 
-            'tricks'=> $trickRepository->findBy([], ['createdAt'=> 'DESC'], 10)
-            ]);
-    }
 
-    #[Route('/create', name: 'app_create')]
-    public function create(): Response
-    {
-      
-        
-
-        return $this->render('trick/create.html.twig');
+        return $this->render('trick/list.html.twig', [
+            'tricks' => $trickRepository->findBy([], ['createdAt' => 'DESC'], 10)
+        ]);
     }
 
     #[Route('/{slug}', name: 'detail')]
     public function detail(Trick $trick): Response
     {
-      
+
         $pictures = $trick->getPictures();
-         
+
         return $this->render('trick/detail.html.twig', [
             'trick' => $trick,
-            'pictures' => $pictures,       
+            'pictures' => $pictures,
         ]);
     }
 
-    #[Route('/edit/{slug}', name: 'edit')]
-    public function edit(Trick $trick): Response
+    #[Route('/create', name: 'app_create')]
+    public function create(Request $request, EntityManagerInterface $entitymanager): Response
     {
-             
-       
+        $trick = new Trick();
+
+        $form = $this->createForm(TrickType::class, $trick);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $trick->setCreatedAt(new \DateTimeImmutable);
+            $trick->setSlug($form->get('name')->getData());
+            $trick->setUser($this->getUser());
+
+            $entitymanager->persist($trick);
+            $entitymanager->flush();
+
+            $this->addFlash(
+                'success',
+                "Votre trick a été posté avec succès"
+            );
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('trick/create.html.twig', [
+            'trickForm' => $form->createView()
+        ]);
+    }
+
+
+    #[Route('/edit/{slug}', name: 'edit')]
+    public function edit(Trick $trick, Request $request, EntityManagerInterface $entitymanager): Response
+    {
+
+        $form = $this->createForm(TrickType::class, $trick);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $trick->setUpdatedAt(new \DateTimeImmutable);
+            $trick->setSlug($form->get('name')->getData());
+            $trick->setUser($this->getUser());
+
+            $entitymanager->persist($trick);
+            $entitymanager->flush();
+
+            $this->addFlash(
+                'success',
+                "Votre trick a été modifié avec succès"
+            );
+
+            return $this->redirectToRoute('app_home');
+        }
+
         return $this->render('trick/edit.html.twig', [
-            'trick'=> $trick   
+            'trickForm' => $form->createView()
         ]);
     }
 
     #[Route('/delete/{slug}', name: 'delete')]
     public function delete(Trick $trick, EntityManagerInterface $entitymanager): Response
     {
+        if(!$trick){
+            $this->addFlash(
+                'danger',
+                "Le trick n\'a pas trouvé"
+            );
+        }
 
         $entitymanager->remove($trick);
         $entitymanager->flush();
 
-        $this->addFlash('success', 'Le trick a bien été supprimé !');
-       
+        $this->addFlash('success', 
+        'Le trick a bien été supprimé !');
+
         return $this->redirectToRoute('app_home');
     }
 }
--- src/Form/TrickType.php
+++ src/Form/TrickType.php
@@ -2,8 +2,12 @@
 
 namespace App\Form;
 
+use App\Entity\Category;
 use App\Entity\Trick;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -24,7 +28,12 @@ class TrickType extends AbstractType
                 ],
                 'label' => 'Description du trick'
                 ])   
-            ->add('category')
+            ->add('category',EntityType::class, [
+                    'class' => Category::class,
+                    'choice_label'=> 'name',
+                    'label'=> 'Categorie du trick',
+                    'required' => true,
+                ]);
         ;
     }
 
